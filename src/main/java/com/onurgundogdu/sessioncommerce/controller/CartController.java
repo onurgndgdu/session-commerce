35b22e34ceec29da3007da7181eea67bafad27d6
gundogduonur <gundogduonur@outlook.com.tr>
2023-08-23 22:47:00 +0300

    added session in controller

---
+++ src/main/java/com/onurgundogdu/sessioncommerce/controller/CartController.java
@@ -0,0 +1,54 @@
+package com.onurgundogdu.sessioncommerce.controller;
+
+
+import com.onurgundogdu.sessioncommerce.model.Product;
+import com.onurgundogdu.sessioncommerce.repository.ProductRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private HttpSession httpSession;
+
+
+    @PostMapping("/add/{productId}")
+    public String addToCart(@PathVariable Long productId) {
+
+        Product product = productRepository.findById(productId).orElse(null);
+
+        if(product != null){
+            List<Product> cartProducts = (List<Product>) httpSession.getAttribute("session");
+            if (cartProducts == null) {
+                cartProducts = new ArrayList<>();
+            }
+            cartProducts.add(product);
+            httpSession.setAttribute("cart", cartProducts);
+
+            return "Product added to cart successfully";
+        }
+        return "Product not found";
+
+    }
+    @GetMapping("/view")
+    public List<Product> viewCart() {
+        List<Product> cartProducts = (List<Product>) httpSession.getAttribute("cart");
+        return cartProducts;
+    }
+    @PostMapping("/checkout")
+    public String checkoutCart() {
+        httpSession.removeAttribute("cart");
+        return "Checkout completed successfully";
+    }
+
+
+}
