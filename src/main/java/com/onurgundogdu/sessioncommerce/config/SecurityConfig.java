b8753757709ce206dca38f166a53a548d4358467
gundogduonur <gundogduonur@outlook.com.tr>
2023-08-23 23:53:43 +0300

    added authenticated and passwordEncoder method

--- src/main/java/com/onurgundogdu/sessioncommerce/config/SecurityConfig.java
+++ src/main/java/com/onurgundogdu/sessioncommerce/config/SecurityConfig.java
@@ -3,12 +3,16 @@ package com.onurgundogdu.sessioncommerce.config;
 import com.onurgundogdu.sessioncommerce.model.User;
 import com.onurgundogdu.sessioncommerce.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -20,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private UserRepository userRepository;
 
     @Override
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         authenticationManagerBuilder.userDetailsService(username -> {
                 User user = userRepository.findByUsername(username);
                 if(user != null) {
@@ -38,5 +42,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     throw  new UsernameNotFoundException("User not found");
                 }
         });
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/register", "/api/login").permitAll()
+                .anyRequest().authenticated();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
