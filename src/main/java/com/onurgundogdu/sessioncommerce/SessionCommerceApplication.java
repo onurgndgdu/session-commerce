56891c33575bdf660c9535a01fa6220e18aa7a62
gundogduonur <gundogduonur@outlook.com.tr>
2023-08-23 22:25:51 +0300

    created pom and app.props

---
+++ src/main/java/com/onurgundogdu/sessioncommerce/SessionCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.onurgundogdu.sessioncommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onurgundogdu/sessioncommerce/SessionCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onurgundogdu.sessioncommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
