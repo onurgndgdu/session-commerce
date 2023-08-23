3930b98856742f48cc1d0b63679cb4e822d55ec6
gundogduonur <gundogduonur@outlook.com.tr>
2023-08-23 23:36:22 +0300

    created loginController and used security

---
+++ src/main/java/com/onurgundogdu/sessioncommerce/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.onurgundogdu.sessioncommerce.controller;
+
+import com.onurgundogdu.sessioncommerce.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user){
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return "User logged in successfully";
+    }
+}
