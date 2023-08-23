05f4a147f09f1e67ecb8432c38fc8e1191e2e3f6
gundogduonur <gundogduonur@outlook.com.tr>
2023-08-23 22:34:30 +0300

    added product class and repository

--- src/test/java/com/onurgundogdu/sessioncommerce/SessionCommerceApplicationTests.java
+++ src/test/java/com/onurgundogdu/sessioncommerce/SessionCommerceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SessionCommerceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
